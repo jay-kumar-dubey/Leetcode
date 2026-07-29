ea6839c9fc3498eafa0d6b33246bd09f19db7e7e
Jay kumar Dubey <148194884+jay-kumar-dubey@users.noreply.github.com>
2026-07-29 22:41:22 +0530

    Time: 436 ms (51.85%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1317-monthly-transactions-i/monthly-transactions-i.sql
@@ -0,0 +1,5 @@
+SELECT TO_CHAR(trans_date, 'YYYY-MM') as month, country , COUNT(*) AS trans_count , SUM(CASE WHEN state = 'approved' THEN 1 ELSE 0 END) AS approved_count , SUM(amount) as trans_total_amount , SUM(CASE WHEN state = 'approved' THEN amount ELSE 0 END) as approved_total_amount
+FROM Transactions
+
+GROUP BY country,
+TO_CHAR(trans_date, 'YYYY-MM')
