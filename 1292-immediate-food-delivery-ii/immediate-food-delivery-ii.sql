de0525b24d0bdea6fa1a2ab258c2f8712ef5b8b1
Jay kumar Dubey <148194884+jay-kumar-dubey@users.noreply.github.com>
2026-08-04 15:58:08 +0530

    Time: 401 ms (47.79%) | Memory: 0B (100.00%) - LeetSync

--- 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
+++ 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
@@ -1,7 +1,10 @@
-# Write your MySQL query statement below
-select 
-round(avg(order_date = customer_pref_delivery_date) * 100,2) immediate_percentage 
-from Delivery
-where (customer_id,order_date) in (select customer_id, min(order_date) from delivery
-               group by customer_id);
-
+-- Write your PostgreSQL query statement below
+SELECT 
+ROUND(AVG(
+    (order_date = customer_pref_delivery_date)::INT
+) * 100, 2 )
+ as immediate_percentage 
+FROM Delivery
+WHERE (customer_id, order_date) IN
+ (SELECT customer_id, MIN(order_date) FROM DELIVERY
+GROUP BY customer_id);
