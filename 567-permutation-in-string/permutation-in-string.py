bc72fbb79e56558a8883948d6e893f9438764188
Jay kumar Dubey <148194884+jay-kumar-dubey@users.noreply.github.com>
2026-08-12 15:43:54 +0530

    Time: 11 ms (96.38%) | Memory: 19.4 MB (18.21%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -1,14 +1,24 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        l = 0
-        s1 = sorted(s1)
-        for r in range(len(s1)-1,len(s2)):
-
-            if s1 == sorted(s2[l:r+1]):
-                return True
-            else:
-                l += 1
+        trail = [0] * 26
+        actual = [0] * 26
+        for i in range(len(s1)):
+            trail[ord(s1[i])- ord('a')] += 1
         
+        l = 0
+
+        for r in range(len(s2)):
+
+            actual[ord(s2[r]) - ord('a')] += 1
+            if r-l + 1 == len(s1):
+                if trail == actual:
+                    return True
+                
+                actual[ord(s2[l])-ord('a')] -= 1
+
+                l+=1
+        
+
         return False
 
 
